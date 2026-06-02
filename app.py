b5748f7b8131d7f2b8b70786a17e820c1e6bed41
Ahmadcodesss <ahmadiftikhar6789@gmail.com>
2026-06-02 21:31:22 +0500

    debug

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from model import summarize_text
 
+print("HF TOKEN:", os.getenv("HF_TOKEN"))
+
+
 load_dotenv()
 
 app = Flask(__name__)
